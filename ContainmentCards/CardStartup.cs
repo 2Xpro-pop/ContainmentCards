82e9571040cfcc8402ae0b9979f3c146628abe8a
2Xpro-pop <bay.asanov@mail.ru>
2023-01-14 12:44:41 +0600

    cardstartup

--- ContainmentCards/CardStartup.cs
+++ ContainmentCards/CardStartup.cs
@@ -7,8 +7,5 @@ using System.Threading.Tasks;
 namespace ContainmentCards;
 public abstract class CardStartup
 {
-    public abstract T GetProperty<T>(string key)
-    {
-        /* Return generic type T. */
-    }
+    public abstract T GetProperty<T>(string key);
 }
